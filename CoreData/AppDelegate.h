c9bb303fec4f5c5d1d193b53f4f265d1cb34771f
ipodishima <marian.paul2@gmail.com>
2012-04-02 16:08:00 +0200

    Initial Commit

---
+++ CoreData/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  CoreData
+//
+//  Created by Marian Paul on 02/04/12.
+//  Copyright (c) 2012 iPuP SARL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ CoreData/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CoreData
+//
+//  Created by Marian Paul on 02/04/12.
+//  Copyright (c) 2012 iPuP SARL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
